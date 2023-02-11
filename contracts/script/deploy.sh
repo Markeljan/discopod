45c8aa6b38eed29848b67325905334395c37f0e0
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2023-02-11 17:50:51 -0500

    add deploy script

---
+++ contracts/script/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Read the RPC URL
+echo Enter Your RPC URL:
+echo Example: "https://eth-mainnet.alchemyapi.io/v2/XXXXXXXXXX"
+read -s rpc
+
+# Read the contract name
+echo Which contract do you want to deploy \(eg Greeter\)?
+read contract
+
+# Read the constructor arguments
+echo Enter constructor arguments separated by spaces \(eg 1 2 3\):
+read -ra args
+
+#Read the Private Key
+echo Enter Your Private Key:
+read -s prvk
+
+forge create ./src/${contract}.sol:${contract} -i --rpc-url $rpc --constructor-args ${args} --legacy --private-key $prvk
