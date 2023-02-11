9488124f08690c87ed59f12ac1a49208326571cd
Markeljan Sokoli <Markeljan19@gmail.com>
2023-02-11 14:02:15 -0500

    latest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,6 @@ const inter = Inter({ subsets: ["latin"] });
 const NAAMA_ADDRESS = "0x2Ba62a52b9244b4E45Ba52cc0B1f8D39B522025D";
 
 export default function Home() {
-  const { config } = usePrepareSendTransaction({
-    request: { to: 'moxey.eth', value: BigNumber.from('10000000000000000') },
-  })
-  
-  const { data, isLoading, isSuccess, sendTransaction } = useSendTransaction(config);
-
   return (
     <div className="h-screen bg-primary">
       <div className="flex-col w-5/6 mx-auto">
@@ -25,10 +19,6 @@ export default function Home() {
             Become a creator and start your own Podchain to earn rewards, connect with your
             audience, and give to public goods.
           </p>
-          <button
-            className="px-4 py-2 mt-4 text-white bg-primaryDark rounded-lg shadow-lg"
-            onClick={() => sendTransaction({ to: NAAMA_ADDRESS })}
-          >
         </div>
       </div>
     </div>
