8eb9180453c9b7d3e9df3cef06afb8d9fe45f558
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2023-02-11 23:13:28 -0500

    update test

--- contracts/test/Pod.t.sol
+++ contracts/test/Pod.t.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 import "../src/Pod.sol";
+import "solmate/tokens/ERC1155.sol";
 import "../lib/forge-std/src/Test.sol";
-contract PodTest is Test {
+contract PodTest is Test, ERC1155TokenReceiver {
     Pod public pod;
     function setUp() public {
         pod = new Pod("https://contracturi.com");
@@ -16,5 +17,5 @@ contract PodTest is Test {
         pod.createPodcast("Podcast Name", "Podcast Description", "Podcast Metadata URI", "podcast topic");
         assertEq(pod.podcastNameToId("Podcast Name"), 1);
     }
-    
+
 }
